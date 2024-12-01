9b517c5d71731ed89a4b1fb45b8be0712c081bfc
Stas <atlantisscale@gmail.com>
2024-12-01 18:03:50 +0300

    начало

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,14 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import {Noto_Sans} from 'next/font/google'
 
+const zen=Noto_Sans({
+  subsets:['cyrillic','latin'],
+  weight:['300','400','500','600','700'],
+  display:'swap',
+  variable:'--font-zen',
+  style:['normal']
+})
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -20,9 +28,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={zen.className} >
+<div style={{height:'110px',weight:'auto',backgroundColor:'red'}}>Руперт</div>
+
         {children}
       </body>
     </html>
